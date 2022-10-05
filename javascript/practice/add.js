9862f108f64ddd934f17836cfe2f1eb4950be681
mechraj <meharajh9@gmail.com>
2022-10-05 21:38:27 +0330

    Adds javascript practice files

---
+++ javascript/practice/add.js
@@ -0,0 +1,6 @@
+console.log(23+97); //adds 2  number
+console.log(23+97+50+30+50+10);  // adds 6  numbers
+let sum=(4+6+9)/77; // defines a variable
+alert(sum); // prints the solution to screen
+let a=10;
+console.log(a);

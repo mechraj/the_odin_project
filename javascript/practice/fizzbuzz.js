8894a9f860e4025620972a21579cfb330ecae584
mechraj <meharajh9@gmail.com>
2022-12-20 21:35:27 +0330

    Adds fizzbuzz file with task and steps to achieve the task

---
+++ javascript/practice/fizzbuzz.js
@@ -0,0 +1,12 @@
+//Task : To write a program that takes input from the user, counts from 1, till the number user entered
+//when it reached the muktiple of 3 it prints out fizz and buzz when it reached multiples of 5
+// If the number is divisible by both 3 and 5 print fizzbuzz
+
+//Plan : Whrn user enters a number
+//start looping from 1
+//If the number is divisible by 3, print fizz
+//If number is divisible by 5 print buzz
+//if number is divisible by both 3 and 5, print fizzbuzz
+
+//Step 1 : ASking user for a input
+let answer=parseInt(prompt('lease enter a number you want to fizz upto: '))

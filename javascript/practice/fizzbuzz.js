1cd49a29a0660b5ead59764e2fad766521bb36cb
mechraj <meharajh9@gmail.com>
2022-12-20 22:18:52 +0330

    numbers divisible by 5 are printed BUZZ

--- javascript/practice/fizzbuzz.js
+++ javascript/practice/fizzbuzz.js
@@ -13,9 +13,13 @@ let answer=parseInt(prompt('Please enter a number you want to fizz upto: '))
 
 //Step 2 :Looping from 1 to the numbner entereed
 for (let i = 1;i<=answer;i++) {
-    //step 3 :If the number is divisible by 3 we print 3
+    //step 3 :If the number is divisible by 3 we print fizz
     if (i%3==0){
         console.log('fizz')
+    }
+    //step 4 :If the number is divisible by 5 we print buzz
+    if (i%5==0) {
+        console.log('Buzz')
     }
     console.log(i);   
 }
